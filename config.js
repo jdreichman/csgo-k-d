7440af3bf54ee1a5e0d2800b855196bf10ed39b7
hzrd(joe) <jdreichman@gmail.com>
2018-09-08 23:05:40 -0400

    need to incorporate ssl...

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = 
     {
-        connectionString : 'mongodb://'+process.env.CS_DATABASE+':'+process.env.CS_DATABASE_PASSWORD+"@"+process.env.CS_BASE_URI + "/" + process.env.CS_DATABASE,
+        connectionString : 'mongodb://'+escape(process.env.CS_DATABASE)+':'+process.env.CS_DATABASE_PASSWORD+"@"+process.env.CS_BASE_URI + "/" + escape(process.env.CS_DATABASE),
     };
 module.exports = config;
