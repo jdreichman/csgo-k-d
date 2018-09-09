14e8f1e6748607dc80942a69dc4f48994141d99f
hzrd(joe) <jdreichman@gmail.com>
2018-09-08 22:12:11 -0400

    need to incorporate ssl...

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ passport.deserializeUser(function(obj, done) {
 
 //Specify Passport authentication strategy (Steam)
 passport.use(new SteamStrategy({
-	returnURL: 'https://'+process.env.CS_BASE_URI+":"+process.env.PORT+'/auth/steam/return',
-	realm: 'https://'+process.env.CS_BASE_URI+":"+process.env.PORT,
+	returnURL: 'http://'+process.env.CS_BASE_URI+":"+process.env.PORT+'/auth/steam/return',
+	realm: 'http://'+process.env.CS_BASE_URI+":"+process.env.PORT,
 	apiKey: process.env.CS_STEAM_API_KEY
 }, function(identifier, profile, done) {
 	//Check if user exists in DB
