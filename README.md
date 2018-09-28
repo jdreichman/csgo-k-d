9b768a69d07ddc1eb63cc7a0df662b6e7ec16333
hzrd(joe) <jdreichman@gmail.com>
2018-09-28 02:44:03 -0400

    omitting code due to client non-payment

--- README.md
+++
@@ -1,2 +0,0 @@
-# csgo-k-d
-transforms kills and deaths in deathmatch mode to a point value.
--- config.js
+++
@@ -1,4 +0,0 @@
-const config = 
-    {
-        connectionString : 'mongodb://heroku_qf3m42l1:l1ikn9umn5gj9rpsk53uaptmhs@ds251112.mlab.com:51112/heroku_qf3m42l1'    };
-module.exports = config;
